7196805cf8c029c246d88c17e553ccdf5300d36d
R0merol <farisfrontiers@gmail.com>
2022-03-10 23:36:40 +0700

    Matrix program 90% complete. Encounters bug when calculating 2x2 inverse matrix

--- main.py
+++ main.py
@@ -19,22 +19,72 @@ def input_matriks():
             matriks[i].append(int(input(f"A {i + 1}, {j + 1}: ")))
     print('-' * 20)
 
-    # Tampilkan matriks A dan B
-    print(">>> Hasil Kalkulasi")
-    print("A:")
-    for i in range(baris):
-        print('\t[', end=' ')
-        for j in range(kolom):
-            print(f"{matriks[i][j]}", end=' ')
-        print(']\n', end='')
-
     return matriks
 
 
 def invers_matriks(matriks):
-    # cari determinan
+    # Cari Determinan
     determinan = cari_determinan(matriks)
-    print(determinan)
+    if determinan == 0:
+        return print("Matrix Singular! (Determinan = 0)")
+
+    # Check if matrix is 2x2 or not
+    kolom = len(matriks)
+    if kolom > 2:
+        # Step 1: Generate Matrix of Minors
+        matriks2 = []
+        if kolom > 2:
+            for i in range(kolom):
+                matriks2.append([])
+                for j in range(kolom):
+                    matriks3 = []
+                    iterasi = 0
+                    for k in [x for x in range(kolom) if x != i]:
+                        matriks3.append([])
+                        for l_ in [y for y in range(kolom) if y != j]:
+                            matriks3[iterasi].append(matriks[k][l_])
+                        iterasi += 1
+                    matriks2[i].append(cari_determinan(matriks3))
+        print_matriks(matriks2, "Step 1:")
+
+        # Step 2: Generate the Matrix of Cofactors
+        for i in range(kolom):
+            for j in range(kolom):
+                if ((i + j) % 2) == 1:
+                    if kolom > 2:
+                        matriks2[i][j] *= -1
+        print_matriks(matriks2, "Step 2:")
+
+        # Step 3: Find the Adjugate/Adjoint
+        matriks3 = []
+        for i in range(kolom):
+            matriks3.append([])
+            for j in range(kolom):
+                if i == j:
+                    matriks3[i].append(matriks2[i][j])
+                else:
+                    matriks3[i].append(matriks2[j][i])
+        print_matriks(matriks3, "Step 3:")
+
+        # Step 4: Divide Adjugate by Determinant
+        for i in range(kolom):
+            for j in range(kolom):
+                matriks3[i][j] /= determinan
+        print_matriks(matriks3, "Step 4:")
+
+        # Print Hasil Invers Matriks
+        print_matriks(matriks3, "A^-1:")
+
+    else:
+        # Step 1:
+        matriks2 = []
+        for i in range(kolom):
+            matriks2.append([])
+            for j in range(kolom):
+                if ((i + j) % 2) == 1:
+                    matriks2[i].append(matriks[i][j] * -1)
+                else:
+                    matriks2[i].append(matriks[i][j])
 
 
 def cari_determinan(matriks):
@@ -51,9 +101,10 @@ def cari_determinan(matriks):
                 iterasi += 1
             determinan = cari_determinan(matriks2)
             if (i % 2) == 0:
-                determinan_akhir -= matriks[0][i] * determinan
-            else:
                 determinan_akhir += matriks[0][i] * determinan
+            else:
+                determinan_akhir -= matriks[0][i] * determinan
+
         return determinan_akhir
     else:
         a = matriks[0][0]
@@ -65,27 +116,57 @@ def cari_determinan(matriks):
 
 
 def test_invers_matriks(matriks):
-    matriks_np = np.array(matriks)
-    matriks_invers = np.linalg.inv(matriks_np)
+    # matriks_np = np.array(matriks)
+    matriks_invers = np.linalg.inv(matriks)
     print(matriks_invers)
 
 
+def print_matriks(matriks, teks=""):
+    kolom = len(matriks)
+    print(f"{teks}")
+    for i in range(kolom):
+        print('\t[', end=' ')
+        for j in range(kolom):
+            print(f"{round(matriks[i][j], 3)}\t", end=' ')
+        print(']\n', end='')
+
+
 def main():
-    # matriks = [[1, 2],
-    #            [3, 4]]
-    matriks = [[2, 2, 3],
-               [4, 5, 6],
-               [7, 8, 9]]
-    # matriks = [[2, 2, 3, 4],
-    #            [5, 6, 7, 8],
+    # matriks = input_matriks()
+    matriks = [[1, 2],
+               [3, 4]]
+    # matriks = [[2, 2, 3],
+    #            [4, 5, 6],
+    #            [7, 8, 9]]
+    # matriks = [[3, 2, 3, 4],
+    #            [5, 6, 10, 8],
     #            [9, 10, 11, 12],
     #            [13, 14, 15, 16]]
 
-    # matriks = input_matriks()
+    # Tampilkan matriks A dan B
+    print(">>> Hasil Kalkulasi")
+    print_matriks(matriks, "A:")
     invers_matriks(matriks)
-    print("-" * 20)
-    test_invers_matriks(matriks)
 
 
 if __name__ == '__main__':
     main()
+
+# Nilai-nilai percobaan
+# matriks = [[1, 2],
+#            [3, 4]]
+# matriks = [[2, 2, 3],
+#            [4, 5, 6],
+#            [7, 8, 9]]
+# matriks = [[3, 2, 3, 4],
+#            [5, 6, 10, 8],
+#            [9, 10, 11, 12],
+#            [13, 14, 15, 16]]
+# matriks = [[3, 2, 3, 4, 1],
+#            [5, 6, 10, 8, 2],
+#            [9, 10, 11, 12, 3],
+#            [13, 14, 15, 16, 4],
+#            [13, 14, 15, 16, 6]]
+# matriks = [[1, 2, 3],
+#            [0, 1, 4],
+#            [5, 6, 0]]
